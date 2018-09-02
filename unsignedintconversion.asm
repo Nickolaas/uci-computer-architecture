6e80d82ccc55e16f59c7944eaf7c409f42d03a2c
Nickolaas <nick_decker5009@elcamino.edu>
2018-09-01 20:07:36 -0500

    unsigned int conversion
    asm

---
+++ unsignedintconversion.asm
@@ -0,0 +1,40 @@
+Nickolas Decker
+CS51 Lab 1
+
+Unsigned Integers Conversion
+
+1. 0b10010011 (Binary)  
+* 147 (Decimal
+* 0x98BD9B (Hexadecimal)
+
+2. 0xD3AD (Hexadecimal)
+* 1101001110101101 (Binary)
+* 54189 (Decimal)
+
+3. 63 (Decimal)
+* 0b111111 (Binary)
+* 0x3F (Hexadecimal
+
+4. 0b00100100 (Binary)
+* 0x24 (Hexadecimal)
+* 36 (Decimal)
+
+5. 0xB33F (Hexadecimal) 
+* 0b1011001100111111 (Binary)
+* 45887 (Decimal)
+
+6. 0 (Decimal)
+* 0x0 (Hexadecimal)
+* 0b0 (Binary)
+
+7. 39 (Decimal)
+* 0b100111 (Binary)
+* 0x27 (Hexadecimal)
+
+8. 0x7EC4 (Hexadecimal)
+* 0b111111011000100 (Binary)
+* 32452 (Decimal)
+
+Two’s Compliment
+1. What is the largest integer (8bit)? +127
+2. How do you represent the numbers 0, 1, and -1? 0 = 00000000, 1 = 00000001, -1 = 11111111
